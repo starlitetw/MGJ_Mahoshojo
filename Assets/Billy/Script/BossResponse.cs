da2aba1465a09371abfc1f160d9a7ddda456480a
BILLY_CJSCOPE\呆呆 <playbilly358@gmail.com>
2019-08-04 13:24:03 +0800

    BOSSATTTACK

---
+++ Assets/Billy/Script/BossResponse.cs
@@ -0,0 +1,33 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.SceneManagement;
+using UnityEngine;
+
+public class BossResponse : MonoBehaviour
+{
+    public Boss_Statu statu;
+    public Transform BulletPos;
+    public GameObject BulletObj;
+    public Animator ani;
+    private void Awake()
+    {
+        statu = this.gameObject.GetComponent<Boss_Statu>();
+    }
+    public void BOSSDead()
+    {
+        SceneManager.LoadScene(statu.SceneName);
+    }
+
+    public void BOSSHurt()
+    {
+        statu.HP--;
+        if (statu.HP >=0)
+        {
+            ani.SetTrigger("Dead");
+        }
+    }
+    public void LaunchBullet()
+    {
+        Instantiate(BulletObj, BulletPos);
+    }
+}
---
+++ Assets/Billy/Script/Boss_Statu.cs
@@ -0,0 +1,12 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Boss_Statu : MonoBehaviour
+{
+    public string SceneName;
+    public int HP;
+    public int Damage;
+    public float MoveSpeed;
+
+}
